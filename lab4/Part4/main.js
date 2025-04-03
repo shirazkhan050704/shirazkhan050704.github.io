60b14810449438b6e2b498bf3c7c99112c03960d
shirazkhan050704 <Shirazkhan050704@gmail.com>
2025-04-02 20:10:43 -0400

    Updated JS
    Main java was updated from added ball detection and ball count

--- lab4/Part4/main.js
+++ lab4/Part4/main.js
@@ -1,7 +1,3 @@
-// Thanks to Renan Martineli for this version of the demo
-
-// setup canvas
-
 const para = document.querySelector('p');
 let count = 0;
 
@@ -11,14 +7,10 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
-// function to generate random number
-
 function random(min,max) {
   const num = Math.floor(Math.random()*(max-min)) + min;
   return num;
 };
-
-// function to generate random RGB color value
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
@@ -159,8 +151,6 @@ class EvilCircle extends Shape {
   }
 
 }
-
-// define array to store balls and populate it
 
 const balls = [];
 
