fceed53c9e6b7678c4f8a8babc7b5a8453934dc1
shirazkhan050704 <Shirazkhan050704@gmail.com>
2025-04-12 13:23:52 -0400

    main.js
    main.js was imported and thats it

--- lab5/main.js
+++ lab5/main.js
@@ -1,5 +1,3 @@
-// functionality for showing/hiding the comments section
-
 const showHideBtn = document.querySelector('.show-hide');
 const commentWrapper = document.querySelector('.comment-wrapper');
 
